abe721ceea45c1f2604b535c1d6967635cc893f5
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2024-08-25 13:43:45 +0700

    Implement a transfer method

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,4 +4,19 @@ class Wallet < ApplicationRecord
   belongs_to :entity
   has_many :sent_transactions, class_name: "Transaction", foreign_key: "source_wallet_id"
   has_many :received_transactions, class_name: "Transaction", foreign_key: "destination_wallet_id"
+
+  def transfer(to:, amount:)
+    return unless self.balance >= amount
+
+    ActiveRecord::Base.transaction do
+      Transaction.create!(
+        amount:,
+        source_wallet_id: self.id,
+        destination_wallet_id: to.id
+      )
+
+      self.balance = self.balance - amount
+      to.balance = to.balance + amount
+    end
+  end
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Wallet, type: :model do
     expect(wallet.entity).to be_present
     expect(created_wallet).to eq wallet
   end
+
+  it "can transfer money to a different wallet" do
+    source_wallet = create :wallet
+    destination_wallet = create :wallet
+
+    source_wallet.transfer(to: destination_wallet, amount: 50_000)
+    transaction = Transaction.find_by(source_wallet_id: source_wallet.id)
+
+    expect(transaction.amount).to eq 50_000
+    expect(source_wallet.balance).to eq 50_000
+    expect(destination_wallet.balance).to eq 150_000
+  end
 end
